deeead4f144003390590bafd6dfd3ed6e0f21190
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-06-27 16:53:36 -0300

    portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header--bg">
+      <nav>
+        <ul>
+          <li>Início</li>
+          <li>Sobre</li>
+          <li>Informações</li>
+          <li>Portfólio</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
